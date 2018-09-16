a2e519eae0f6aa5cdaed64c296294dcfa9d670b0
Brayden <eternumhd111@gmail.com>
2018-09-16 19:01:02 +1000

    *Testing Modifcations Made to Main Form*

--- NetworkArithmeticGame/NetworkArithmeticGame/Form1.cs
+++ NetworkArithmeticGame/NetworkArithmeticGame/Form1.cs
@@ -202,6 +202,7 @@ namespace NetworkArithmeticGame
                 sb.Append(value + nodeList.getCurrentNode().tostring());
             }
             lstLinkedList.Text = sb.ToString();
+            sb.Append("TAIL <-> ");
         }
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
